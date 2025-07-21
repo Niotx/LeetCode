925360350188e68772bd11fe12fcad0b4436b5fb
Niotx <112319254+Niotx@users.noreply.github.com>
2025-07-21 21:35:18 +0330

    Update product-of-array-except-self.rs

--- product-of-array-except-self.rs
+++ product-of-array-except-self.rs
@@ -1,1 +1,20 @@
 
+impl Solution {
+    pub fn product_except_self(nums: Vec<i32>) -> Vec<i32> {
+        let mut answer = vec![];
+        let mut mul = 0;
+        let mut counter = 0;
+        for j in 0..nums.len(){
+                
+                if j != counter {
+                    mul += nums[j] * nums[counter];
+                    print!("{} --",mul);
+                    counter += 1;
+                }   
+            answer.push(mul);
+            mul = 0;
+            counter = 0;
+        }
+        return answer;
+    }
+}
