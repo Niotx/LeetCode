f4674b71b6cd6a10241778fbe3f4193debd9fb95
Niotx <112319254+Niotx@users.noreply.github.com>
2025-08-19 16:48:08 +0330

    Create equal-row-and-column-pairs.rs

---
+++ equal-row-and-column-pairs.rs
@@ -0,0 +1,18 @@
+impl Solution {
+    pub fn equal_pairs(grid: Vec<Vec<i32>>) -> i32 {   
+        let mut c = 0;
+        for i in 0..grid.len(){
+            let mut v:Vec<i32> = vec![];
+            for j in 0..grid.len(){
+                v.push(grid[j][i])
+            }
+            print!("{:?}",v);
+            for k in 0..grid.len(){
+                if v == grid[k]{
+                    c += 1
+                }
+            }  
+        }
+        c
+    }
+}
